2bb665d2c10941bfceea1c7bdefe270c75831a55
Wyatt Frelot <wjfrelo@users.noreply.github.com>
2021-08-29 09:45:09 -0400

    Create variables.tf

---
+++ Lesson7/variables.tf
@@ -0,0 +1,17 @@
+variable "access_key" {
+	description = "access_key for aws"
+	type        = string
+	default     = ""
+}
+
+variable "secret_key" {
+	description = "secret_key for aws"
+	type        = string
+	default     = ""
+}
+
+variable "session_token" {
+	description = "session_token for aws"
+	type        = string
+	default     = ""
+}
