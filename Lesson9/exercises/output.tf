92859a2cbf06d4a7dd35b5768e21eb8671637ff9
Wyatt Frelot <wjfrelo@users.noreply.github.com>
2021-09-05 09:27:36 -0400

    Create output.tf

---
+++ Lesson9/exercises/output.tf
@@ -0,0 +1,16 @@
+output "private-key" {
+  value = tls_private_key.private-key.private_key_pem
+  sensitive = true
+}
+
+output "public-key-openssh" {
+  value = tls_private_key.private-key.public_key_openssh
+}
+
+output "web-0" {
+  value = aws_instance.web.*.public_ip[0]
+}
+
+output "web-1" {
+  value = aws_instance.web.*.public_ip[1]
+}
